2fd51a3133d336a94da2a90e17f4a69691fcfb73
June <sjunepeters@gmail.com>
2026-06-16 13:16:20 -0700

    build: faculty schoology pages

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -17,10 +17,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://jpeters-uprep.github.io",
+  url: "https://makers.universityprep.org/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/uprep-academic-tech-docs/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
